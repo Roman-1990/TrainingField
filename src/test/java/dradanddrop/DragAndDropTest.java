1e86c4ea28e0f43fed529b40417862dfd406fe59
Roman <pcuxman@yandex.ru>
2021-05-27 16:15:17 +0300

    Add homework #2-3 by lesson №4

--- src/test/java/dradanddrop/DragAndDropTest.java
+++ src/test/java/dradanddrop/DragAndDropTest.java
@@ -1,6 +1,7 @@
 package dradanddrop;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
@@ -16,6 +17,14 @@ public class DragAndDropTest {
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
         sleep(5000);
+
+    }
+    //нет понимания, как заполнять внутренности, элемент не переносится
+    @Test
+    void movingActionsTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        actions().moveToElement(element("#column-a")).clickAndHold(element("#column-a")).moveToElement(element("#column-b")).release().perform();
 
     }
 }
